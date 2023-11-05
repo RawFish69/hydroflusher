d2339830a94de4ae5861e03c5b7db782476b2d1c
RawFish69 <108964215+RawFish69@users.noreply.github.com>
2023-11-04 21:56:04 -0500

    Update pump_single_plot.py

--- pump_single_plot.py
+++ pump_single_plot.py
@@ -2,10 +2,6 @@
 Script Name: PumpPlot
 Description: Capstone Project Computation - Create single plot for pump & system curve
 """
-"""
-Script Name: PumpPlot
-Description: Capstone Project Computation - Create single plot for pump & system curve
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -24,9 +20,17 @@ def system_curve(Q, h0, k):
     return h0 + k * Q ** 2
 
 
-def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.02, Q_shift=15, Q_accumulator_shift=20, filename="figure.png"):
+def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.02, Q_shift=15, Q_accumulator_shift=20, volume=0, discharge_time=0,
+                            filename="figure.png"):
     Q = np.linspace(0, Q_max * 2, 400)
     Q -= Q_shift
+    pump_curves = [
+        pump_curve(Q, Q_max, H_max),
+        pump_curve(Q, Q_max, H_max * 0.5) + pump_curve(Q, Q_max, H_max * 0.5),
+        pump_curve(Q, Q_max, H_max) + pump_curve(Q, Q_max, H_max * 0.5),
+        pump_curve(Q, Q_max, H_max) + pump_curve(Q, Q_max, H_max)
+    ]
+
     H_single = pump_curve(Q, Q_max, H_max)
     H_5050 = pump_curve(Q, Q_max, H_max * 0.5) + pump_curve(Q, Q_max, H_max * 0.5)
     H_10050 = pump_curve(Q, Q_max, H_max) + pump_curve(Q, Q_max, H_max * 0.5)
@@ -34,25 +38,28 @@ def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.02, Q_shift=15, Q_ac
     H_system = system_curve(Q, h, k)
     plt.figure(figsize=(8, 8))
 
+    # Define colors for each curve to use in the plot and legend
     pump_colors = ['blue', 'blue', 'blue', 'blue']
     accumulator_colors = ['cyan', 'cyan', 'cyan', 'cyan']
 
+    # Plotting pump curves
     plt.plot(Q, H_single, color=pump_colors[0], label='Single Pump')
     plt.plot(Q, H_5050, color=pump_colors[1], label='50%+50% Pumps')
     plt.plot(Q, H_10050, color=pump_colors[2], label='100%+50% Pumps')
     plt.plot(Q, H_100100, color=pump_colors[3], label='100%+100% Pumps')
 
-    Q_acc = Q + Q_accumulator_shift  # Apply shift for accumulator
+    # Plotting accumulator curves with shifted Q for the accumulator effect
+    Q_acc = Q + Q_accumulator_shift
     plt.plot(Q_acc, H_single, '--', color=accumulator_colors[0], label='Single Pump + Acc')
     plt.plot(Q_acc, H_5050, '--', color=accumulator_colors[1], label='50%+50% Pumps + Acc')
     plt.plot(Q_acc, H_10050, '--', color=accumulator_colors[2], label='100%+50% Pumps + Acc')
     plt.plot(Q_acc, H_100100, '--', color=accumulator_colors[3], label='100%+100% Pumps + Acc')
 
+    # Plotting system curve
     plt.plot(Q, H_system, 'r-', label='System Curve')
 
     # Calculating intersection points and creating legend entries
     custom_legend_handles = []
-    pump_curves = [H_single, H_5050, H_10050, H_100100]
     curve_names = ["Single Pump", "50%+50% Pumps", "100%+50% Pumps", "100%+100% Pumps"]
 
     for H_pump, name, pump_color, acc_color in zip(pump_curves, curve_names, pump_colors, accumulator_colors):
@@ -66,7 +73,8 @@ def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.02, Q_shift=15, Q_ac
                                                 label=f'{name}: Q={q_value_pump:.2f} m³/h, Head={head_value_pump:.2f} m'))
 
         # Intersection for Pump + Accumulator
-        diff_acc_system = H_pump - H_system
+        diff_acc_system = H_pump + accumulator_shift(volume,
+                                                     discharge_time) - H_system  # Use adjusted head for accumulator
         idx_int_acc = np.where(diff_acc_system[:-1] * diff_acc_system[1:] <= 0)[0][-1]
         q_value_acc = Q_acc[idx_int_acc]
         head_value_acc = H_system[idx_int_acc]
@@ -74,9 +82,11 @@ def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.02, Q_shift=15, Q_ac
         custom_legend_handles.append(plt.Line2D([], [], color=acc_color, marker='o', linestyle='--',
                                                 label=f'{name} + Acc: Q={q_value_acc:.2f} m³/h, Head={head_value_acc:.2f} m'))
 
+    # Adding the system curve to the legend
     custom_legend_handles.append(plt.Line2D([], [], color='red', linestyle='-', label='System Curve'))
 
-    accumulator_info = f"Accumulator Volume Shift: {Q_accumulator_shift}"
+    # Adding the accumulator volume to the legend
+    accumulator_info = f"Accumulator Volume: {volume:.2f} m³, Discharge interval: {discharge_time}/ hour"
     custom_legend_handles.append(plt.Line2D([], [], color='none', label=accumulator_info))
 
     # Plotting the custom legend
@@ -98,15 +108,15 @@ def generate_combinations_plot(volumes, discharge_times, Q_max=150, H_max=500, h
     for volume in volumes:
         for discharge_time in discharge_times:
             shift = accumulator_shift(volume, discharge_time)
-            file_name = f"plots/pump_system_Q_{Q_max}_H_{H_max}_shift_{Q_shift}_{iteration}.png"
+            file_name = f"pump_system_Q_{Q_max}_H_{H_max}_shift_{Q_shift}_{iteration}.png"
             iteration += 1
-            plot_pump_system_curves(Q_max, H_max, h, k, Q_shift, shift, file_name)
+            plot_pump_system_curves(Q_max, H_max, h, k, Q_shift, shift, volume, discharge_time, file_name)
             time.sleep(1)
 
 
 def main():
     volumes = [0, 1, 3, 5, 10, 20, 30, 40, 50]  # Example volumes
-    discharge_times = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # Example discharge times
+    discharge_times = [0, 1, 2, 3, 5, 10, 15, 20, 25, 30]  # Example discharge times
     generate_combinations_plot(volumes, discharge_times)
 
 main()
