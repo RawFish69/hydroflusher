879b35f06862dca82fdb7c0f6a079607978c8514
RawFish69 <108964215+RawFish69@users.noreply.github.com>
2023-11-19 18:31:44 -0600

    Update pump_single_plot.py

--- src/pump_single_plot.py
+++ src/pump_single_plot.py
@@ -25,23 +25,24 @@ def system_curve(Q, h0, k):
     return h0 + k * Q ** 2
 
 
-def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.01, Q_shift=15, Q_accumulator_shift=20, volume=0, discharge_time=0,
+def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.0025, Q_shift=15, Q_accumulator_shift=20, volume=0, discharge_time=0,
                             filename="figure.png"):
     Q = np.linspace(0, Q_max * 2, 400)
     Q -= Q_shift
     pump_curves = [
         pump_curve(Q, Q_max, H_max),
-        pump_curve(Q, Q_max, H_max * 0.70) + pump_curve(Q, Q_max, H_max * 0.70),
-        pump_curve(Q, Q_max, H_max * 0.80) + pump_curve(Q, Q_max, H_max * 0.80),
-        pump_curve(Q, Q_max, H_max * 0.90) + pump_curve(Q, Q_max, H_max * 0.90),
-        pump_curve(Q, Q_max, H_max) + pump_curve(Q, Q_max, H_max)
+        pump_curve(Q / 1.4, Q_max, H_max),
+        pump_curve(Q / 1.6, Q_max, H_max),
+        pump_curve(Q / 1.8, Q_max, H_max),
+        pump_curve(Q / 2, Q_max, H_max)
     ]
 
+
     H_single = pump_curve(Q, Q_max, H_max)
-    H_7070 = pump_curve(Q, Q_max, H_max * 0.70) + pump_curve(Q, Q_max, H_max * 0.70)
-    H_8080 = pump_curve(Q, Q_max, H_max * 0.80) + pump_curve(Q, Q_max, H_max * 0.80)
-    H_9090 = pump_curve(Q, Q_max, H_max * 0.90) + pump_curve(Q, Q_max, H_max * 0.90)
-    H_100100 = pump_curve(Q, Q_max, H_max) + pump_curve(Q, Q_max, H_max)
+    H_7070 = pump_curve(Q / 1.4, Q_max, H_max)
+    H_8080 = pump_curve(Q / 1.6, Q_max, H_max)
+    H_9090 = pump_curve(Q / 1.8, Q_max, H_max)
+    H_100100 = pump_curve(Q / 2, Q_max, H_max)
     H_system = system_curve(Q, h, k)
     plt.figure(figsize=(8, 8))
 
@@ -53,16 +54,16 @@ def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.01, Q_shift=15, Q_ac
     plt.plot(Q, H_single, color=pump_colors[0], label='Single Pump')
     plt.plot(Q, H_7070, color=pump_colors[1], label='70%+70% Pumps')
     plt.plot(Q, H_8080, color=pump_colors[2], label='80%+80% Pumps')
-    plt.plot(Q, H_9090, color=pump_colors[2], label='90%+90% Pumps')
-    plt.plot(Q, H_100100, color=pump_colors[3], label='100%+100% Pumps')
+    plt.plot(Q, H_9090, color=pump_colors[3], label='90%+90% Pumps')
+    plt.plot(Q, H_100100, color=pump_colors[4], label='100%+100% Pumps')
 
     # Plotting accumulator curves with shifted Q for the accumulator effect
     Q_acc = Q + Q_accumulator_shift
     plt.plot(Q_acc, H_single, '--', color=accumulator_colors[0], label='Single Pump + Acc')
     plt.plot(Q_acc, H_7070, '--', color=accumulator_colors[1], label='70%+70% Pumps + Acc')
     plt.plot(Q_acc, H_8080, '--', color=accumulator_colors[2], label='80%+80% Pumps + Acc')
-    plt.plot(Q_acc, H_9090, '--', color=accumulator_colors[2], label='90%+90% Pumps + Acc')
-    plt.plot(Q_acc, H_100100, '--', color=accumulator_colors[3], label='100%+100% Pumps + Acc')
+    plt.plot(Q_acc, H_9090, '--', color=accumulator_colors[3], label='90%+90% Pumps + Acc')
+    plt.plot(Q_acc, H_100100, '--', color=accumulator_colors[4], label='100%+100% Pumps + Acc')
 
     # Plotting system curve
     plt.plot(Q, H_system, 'r-', label='System Curve')
@@ -99,10 +100,12 @@ def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.01, Q_shift=15, Q_ac
     custom_legend_handles.append(plt.Line2D([], [], color='none', label=accumulator_info))
 
     # Plotting the custom legend
-    plt.legend(handles=custom_legend_handles, loc='best', fontsize=8)
+    # plt.legend(handles=custom_legend_handles, loc='best', fontsize=8)
+    plt.legend(handles=custom_legend_handles, loc='upper right', fontsize=8)
+    plt.ylim([0, H_max*1.4])
     plt.xlabel('Flow Rate (m³/h)')
     plt.ylabel('Head (m)')
-    plt.title('Pump and System Curves with Accumulator Effect')
+    plt.title('plots/Pump and System Curves with Accumulator Effect')
     plt.grid(True)
     plt.tight_layout()
     plt.savefig(filename)
@@ -112,12 +115,12 @@ def plot_pump_system_curves(Q_max=300, H_max=300, h=30, k=0.01, Q_shift=15, Q_ac
 
 # Remove any previous annotations code related to the adjust_text_position function or any calls to it.
 
-def generate_combinations_plot(volumes, discharge_times, Q_max=150, H_max=500, h=20, k=0.015, Q_shift=20):
+def generate_combinations_plot(volumes, discharge_times, Q_max=150, H_max=200, h=20, k=0.0025, Q_shift=20):
     iteration = 1
     for volume in volumes:
         for discharge_time in discharge_times:
             shift = accumulator_shift(volume, discharge_time)
-            file_name = f"plots/pump_system_Q_{Q_max}_H_{H_max}_shift_{Q_shift}_{iteration}.png"
+            file_name = f"output/plots/pump_system_Q_{Q_max}_H_{H_max}_shift_{Q_shift}_{iteration}.png"
             iteration += 1
             plot_pump_system_curves(Q_max, H_max, h, k, Q_shift, shift, volume, discharge_time, file_name)
             time.sleep(1)
